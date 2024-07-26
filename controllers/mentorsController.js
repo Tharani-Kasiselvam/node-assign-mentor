775ab3d0cefc4abfaed0378047fa8597eeead49a
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-07-26 06:19:44 +0530

    mangoose findOne to update StudentsList

--- controllers/mentorsController.js
+++ controllers/mentorsController.js
@@ -1,4 +1,5 @@
 const mentors = require('../models/mentors')
+const students = require('../models/students')
 
 const mentorsController = {
     createMentor : async (req,res) => {
@@ -16,6 +17,21 @@ const mentorsController = {
         }catch(error){
             res.status(500).json({message:"ERROR while uploading Mentor details"})
 
+        }
+    },
+
+    updateStudentForMentor : async (req,res) => {
+        try{
+            // const { id } = request.params;
+            const {mentor_id,students_list} = req.body
+            const mentor_info = await mentors.findOne({"mentor_id":mentor_id})
+            mentor_info.stud_list = students_list
+
+            const updateStudentsList = await mentor_info.save()
+
+            res.json({message: mentor_info.stud_list})
+        }catch(error){
+            res.status(500).json({message:"ERROR while adding Student details with Mentor"})
         }
     }
 }
--- routes/assignMentorRoutes.js
+++ routes/assignMentorRoutes.js
@@ -7,4 +7,6 @@ const studentsController = require('../controllers/studentsController')
 router.post('/createMentor',mentorsController.createMentor)
 router.post('/createStudent',studentsController.createStudent)
 
+router.put('/updateStudentForMentor',mentorsController.updateStudentForMentor)
+
 module.exports = router
